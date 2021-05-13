8cbf48cb5b294bc367fb51cdaede1e5981c1a5ac
shadow7L <42204012+shadow7L@users.noreply.github.com>
2021-05-14 02:53:38 +0800

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -5371,7 +5371,7 @@ function(e, t, n) {
     d = n(14);
     n(118);
     var g = n(123),
-    m = ["https://shadowq.com/data/cache/forum.js","https://shadowq.com/data/cache/editor.js","https://shadowq.com/data/cache/style_100_common.css","https://shadowq.com/template/shadowq_ui/shadowq_img/logo.svg"],
+    m = ["https://shadowq.com/data/cache/forum.js","https://shadowq.com/data/cache/editor.js","https://shadowq.com/data/cache/style_100_common.css","https://shadowq.com/data/cache/style_100_portal_index.css"],
     y = function() {
         function e(e) {
             this.diagnoseIndex = 0,
